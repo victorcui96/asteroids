bf9f8ced1f9bb40034eccbdc7966d18e47ac98a2
Victor Cui <victor.cui1996@gmail.com>
2017-02-15 01:34:24 -0500

    Previously, Phaser was not loaded properly. Just loaded Phaser in from a CDN.

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,6 +1,6 @@
 <template>
-	<div>
-		<h1>Test</h1>
+	<div id="game-container">
+		
 	</div>
 </template>
 
@@ -10,8 +10,28 @@ export default {
   name: 'Game',
 
   mounted () {
-    // greate phaser game here
-    // var game = new window.Phaser.Game(800, 800, window.Phaser.AUTO)
+    // create phaser game here
+    console.log('Game ->mounted')
+    // Create an instace of a Phaser.Game object
+    // First two parameters are the width & height of the <canvas> element that Phaser creates
+    // Third parameter = rendering context, either WEBGL or Cnvas
+    // var test = new window.Phaser.Game()
+    var game = new window.Phaser.Game(800, 800, window.Phaser.AUTO, 'game-container', {
+      preload: preload, create: create, update: update
+    })
+
+    // preload (), create(), and update() are essential Phaser functions
+
+    function preload () {
+      // game.load.image('logo', '../assets/logo.png')
+    }
+    function create () {
+      console.log('hey')
+      game.add.sprite(0, 0, 'star')
+    }
+    function update () {
+
+    }
   },
 
   data () {

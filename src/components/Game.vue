992fbb7be8f9bebfe88f77e652f6a550b1803101
Victor Cui <victor.cui1996@gmail.com>
2017-02-15 14:29:12 -0500

    Trying to figure out why my image assets are not loading using Phaser.

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,6 +1,6 @@
 <template>
 	<div id="game-container">
-		
+		<img src="../assets/sky.png" alt="">
 	</div>
 </template>
 
@@ -14,20 +14,28 @@ export default {
     console.log('Game ->mounted')
     // Create an instace of a Phaser.Game object
     // First two parameters are the width & height of the <canvas> element that Phaser creates
-    // Third parameter = rendering context, either WEBGL or Cnvas
-    // var test = new window.Phaser.Game()
-    var game = new window.Phaser.Game(800, 800, window.Phaser.AUTO, 'game-container', {
+    // Third parameter = rendering context, either WEBGL or Canvas
+    // var winW = document.body.offsetWidth
+    // var winH = document.body.offsetHeight
+    var game = new window.Phaser.Game(1410, 800, window.Phaser.AUTO, 'game-container', {
       preload: preload, create: create, update: update
     })
 
     // preload (), create(), and update() are essential Phaser functions
 
     function preload () {
-      // game.load.image('logo', '../assets/logo.png')
+      game.load.image('sky', 'src/assets/sky.png')
+      game.load.image('spaceship', '../assets/spaceship.jpg')
+      game.load.image('logo', './logo.png')
+      var loc = window.location.pathname
+      // var dir = loc.substring(0, loc.lastIndexOf('/'))
+      console.log(loc)
+      game.load.start()
     }
     function create () {
       console.log('hey')
-      game.add.sprite(0, 0, 'star')
+      // Add a sprite to the game
+      game.add.sprite(0, 0, 'spaceship')
     }
     function update () {
 
@@ -43,4 +51,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+#game-container {
+	background: gray;
+}
 </style>	
